92ed5429bef160f4560125014a8719fce721727b
Lucas Norrflod <lucas@norrflod.se>
2026-02-27 21:12:41 +0100

    Integrate SD card functionality for data logging
    Added functionality to read from and write to an SD card, including data logging of MPU6050 angles, pressure, altitude, and temperature.

--- arduino_c_code/sender/sender.ino
+++ arduino_c_code/sender/sender.ino
@@ -1,31 +1,43 @@
 // import libraries
 #include "Wire.h"
-//#include <MPU6050_light.h>
+#include <MPU6050_light.h>
 #include <Adafruit_BMP085.h>
 #define seaLevelPressure_hPa 1013.25
+#include <SD.h>
 
+File myFile;
 Adafruit_BMP085 bmp;
-//MPU6050 mpu(Wire);
+MPU6050 mpu(Wire);
 
 // initialize variables
 float angleX;
 float angleY;
+const int chipSelect = 9;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600); //Remove serial when 
-  //Wire.begin();
+  Serial.begin(9600); 
+  Wire.begin();
+  byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
 
-  //byte status = mpu.begin();
-  //Serial.print(F("MPU6050 status: "));
-  //Serial.println(status);
-  //while (status != 0) {}  // stop everything if could not connect to MPU6050
-  //mpu.calcOffsets();  // gyro and accelero
-  // Serial.println(F("Calculating offsets, do not move MPU6050"));
-  // delay(1000);
-  // // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
-  // mpu.calcOffsets();  // gyro and accelero
-  // Serial.println("Done!\n");
+  // Inititalizing SD-card
+  if (!SD.begin(chipSelect)) {
+    while (1);
+  }
+
+  // Removes data.txt if it exists on SD-card
+  if (SD.exists("data.txt")) {
+    SD.remove("data.txt");
+  } 
+
+  while (status != 0) {}  // stop everything if could not connect to MPU6050
+  mpu.calcOffsets();  // gyro and accelero
+   Serial.println(F("Calculating offsets, do not move MPU6050"));
+   delay(1000);
+   // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
+   mpu.calcOffsets();  // gyro and accelerometer
+   Serial.println("Done!\n");
   Serial.begin(9600);
     if (!bmp.begin()){
       Serial.println("bmp not found!");
@@ -34,29 +46,34 @@ void setup() {
 }
 
 void loop() {
-  //mpu.update();
-  //float angleX = mpu.getAngleX();
-  //float angleY = mpu.getAngleY();
-  //float angleZ = mpu.getAngleZ();
-  //Serial.print("X: ");
-  //Serial.println(angleX);
-  //serial.print("Y: ");
-  //Serial.println(angleY);
-  //Serial.print("Z: ")
-  //Serial.println(angleZ);
-  Serial.print("Pressure: ");
-  Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
 
-  Serial.print("Altitude = ");
-  Serial.print(bmp.readAltitude());
-  Serial.println(" meters");
+  myFile = SD.open("data.txt", FILE_WRITE);
+  mpu.update();
+  float angleX = mpu.getAngleX();
+  float angleY = mpu.getAngleY();
+  float angleZ = mpu.getAngleZ();  
 
-  Serial.print("Pressure at sealevel (calculated) = ");
-  Serial.print(bmp.readSealevelPressure());
-  Serial.println(" Pa");
+  float pressure = bmp.readPressure();
 
-  Serial.print("Temperature = ");
-  Serial.print(bmp.readTemperature());
-  Serial.println(" *C");
+  int altitude = bmp.readAltitude();
+
+
+  float temp = bmp.readTemperature();
+  myFile.print(angleX);
+  myFile.print(",");
+  myFile.print(angleY);
+  myFile.print(",");
+  myFile.print(angleZ);
+  myFile.print(",");
+  myFile.print(pressure);
+  myFile.print(",");
+  myFile.print(altitude);
+  myFile.print(",");
+  myFile.println(temp);
+  myFile.close();
+  myFile = SD.open("data.txt", FILE_READ);
+  while(myFile.available()) {
+    Serial.print(myFile.read());
+  }
+  delay(1000);
 }
